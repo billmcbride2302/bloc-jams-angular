340713659ca5070dbb455daf90108d09daec5dd6
Bill McBride <bill@Bills-MacBook-Pro.local>
2017-05-11 07:35:52 -0400

    checkpoint 7

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,8 +1,9 @@
 (function() {
-   function CollectionCtrl(Fixtures) {
-     this.albums = Fixtures.getCollection(12);
+  function CollectionCtrl(Fixtures) {
+    this.albums = Fixtures.getCollection(12);
+  }
 
-     angular
-         .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
- })();
+  angular
+    .module('blocJams')
+    .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
+})();
